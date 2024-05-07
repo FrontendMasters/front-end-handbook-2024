81e0f694d4a72bc72e64322d42e74b7718ce3975
Serkan USLU <serkanuslu.info@gmail.com>
2024-05-07 21:38:26 +0300

    fix: build fix, clear broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/chapter-01',
               },
             ],
           },
